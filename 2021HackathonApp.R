910616897952c3a94100711da739a154dd6b7e50
blackrd1 <92227352+blackrd1@users.noreply.github.com>
2021-10-09 13:47:26 -0500

    Add files via upload

---
+++ 2021HackathonApp.R
@@ -0,0 +1,14 @@
+library(shiny)
+
+# Define UI ----
+ui <- fluidPage(
+    titlePanel("2021 Hackathon App: By Riley Black and Chloe Hall"),
+)
+
+# Define server logic ----
+server <- function(input, output) {
+    
+}
+
+# Run the app ----
+shinyApp(ui = ui, server = server)
