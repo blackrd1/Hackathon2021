a61cbf679b573302f9818e3f813f6e7a2a4fa091
chloe-hall <92227561+chloe-hall@users.noreply.github.com>
2021-10-09 22:57:47 -0500

    Changed Unit Description

--- 2021HackathonApp.R
+++ 2021HackathonApp.R
@@ -47,38 +47,37 @@ names(nashdat)[names(nashdat)=='project']<-"projectname"
 
 # Define UI ----
 ui <- fluidPage(
-  
-  titlePanel(h3("Riley Black and Chloe Hall's Vandy Hackathon Project")),
-  
-  mainPanel(
-    h1("Low-Income Housing Tax Credit Properties Map"),
-    fluidRow("This interactive map shows the distribution of LIHTC properties built in Nashville from 1987-2019."),
-    br(),
-    tabsetPanel(
-      tabPanel("Nashville Cluster", leafletOutput("NashvilleClusterMap"),
-               h4(
-                 "These clusters display areas of higher concentration of LIHTC developments. Click on a cluster or zoom in to see how developments are distributed!")),
-      
-      tabPanel("Nashville Marker", leafletOutput("NashvilleMarkerMap"),
-               h4(
-                 "These markers display each LIHTC property.Click on a marker to see the number of units and the tax dollars allocated!")),
-      tabPanel("Moon", leafletOutput("MoonMap"),
-               h3(
-                 "There are no LIHTC properties on the moon."),
-               h6("(yet)"))
+    
+    titlePanel(h3("Riley Black and Chloe Hall's Vandy Hackathon Project")),
+    
+    mainPanel(
+        h1("Low-Income Housing Tax Credit Properties Map"),
+        fluidRow("This interactive map shows the distribution of LIHTC properties built in Nashville from 1987-2019."),
+        br(),
+        tabsetPanel(
+            tabPanel("Nashville Cluster", leafletOutput("NashvilleClusterMap"),
+                     h4(
+                         "These clusters display areas of higher concentration of LIHTC developments. Click on a cluster or zoom in to see how developments are distributed!")),
+            
+            tabPanel("Nashville Units", leafletOutput("NashvilleMarkerMap"),
+                     h4(
+                         "These markers display the number of affordable units in each LIHTC property.Click on a marker to see the property and the tax dollars allocated!")),
+            tabPanel("Moon", leafletOutput("MoonMap"),
+                     h3(
+                         "There are no LIHTC properties on the moon."),
+                     h6("(yet)"))
+        ),
+        fluidRow("This data comes from the HUD National Low Income Housing Tax Credit (LIHTC) Database 1987-2019 Data Dictionary."),
+        fluidRow("For more information, visit https://www.huduser.gov/portal/datasets/lihtc.html")
     ),
-    fluidRow("This data comes from the HUD National Low Income Housing Tax Credit (LIHTC) Database 1987-2019 Data Dictionary."),
-    fluidRow("For more information, visit https://www.huduser.gov/portal/datasets/lihtc.html")
-  ),
-  sidebarPanel(
-    h4("What is a LIHTC?"),
-    fluidRow("Low-Income Housing Tax credits are funds given to private developers by the state or local government to finance qualifying housing developments that fit guidelines of affordability and create new rental units for low income tenants."), 
-    br(),
-    fluidRow("This program helps fill the market need for low income housing that is often unadressed by the private development market.")
-  )
-  
+    sidebarPanel(
+        h4("What is a LIHTC?"),
+        fluidRow("Low-Income Housing Tax credits are funds given to private developers by the state or local government to finance qualifying housing developments that fit guidelines of affordability and create new rental units for low income tenants."), 
+        br(),
+        fluidRow("This program helps fill the market need for low income housing that is often unadressed by the private development market.")
+    )
+    
 )
-
 # Define server logic ----
 server <- function(input, output, session) {
   
