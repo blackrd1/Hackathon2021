716adfa3f3b87ccf3666ae8aa1b08bf1b9ae08ac
blackrd1 <92227352+blackrd1@users.noreply.github.com>
2021-10-09 20:02:51 -0500

    Vandy point added

--- 2021HackathonApp.R
+++ 2021HackathonApp.R
@@ -13,32 +13,29 @@ library(shiny)
 library(spDataLarge)
 library(RColorBrewer)
 
-
-setwd("~/Downloads/LIHTC")
-
-LIHTCPUB <- read_csv("LIHTCPUB.CSV")
+#ADD WORKING DIRECTORY AND LOADING OF CSV DATA IN HERE!!!!!!!!!!!!
 dat <- LIHTCPUB
 remove(LIHTCPUB)
 
 #Filters to only Tennessee
 nashdat <- dat %>%
-    filter(proj_st=="TN")
+  filter(proj_st=="TN")
 
 #Filters to only Nashville data
 nashdat <- nashdat %>%
-    filter(proj_cty=="NASHVILLE")
+  filter(proj_cty=="NASHVILLE")
 
 #Dropped NA for Project Addresses
 nashdat <- nashdat%>%
-    drop_na(proj_add)
+  drop_na(proj_add)
 
 #Dropped NA for Project Zipcodes
 nashdat <- nashdat%>%
-    drop_na(proj_zip)
+  drop_na(proj_zip)
 
 #Filtered out the data points without years
 nashdat <- nashdat%>%
-    filter(yr_pis!= 9999 & yr_pis!= 8888)
+  filter(yr_pis!= 9999 & yr_pis!= 8888)
 
 
 #Copied from rstudio page on leaflet
@@ -50,68 +47,72 @@ names(nashdat)[names(nashdat)=='project']<-"projectname"
 
 # Define UI ----
 ui <- fluidPage(
-    
-    titlePanel(h1("Riley Black and Chloe Hall's Hackathon Map")),
+  
+    titlePanel(h1("Riley Black and Chloe Hall's Hackathon Project")),
     
     mainPanel(
-        h3("Low-Income Housing Tax Credit Properties Map"),
-        fluidRow("This map shows the distribution of low-income properties built in Nashville from 1987-2019."),
-        tabsetPanel(
-            tabPanel("Nashville Cluster", leafletOutput("NashvilleClusterMap"),
-                     fluidRow(
-                         "These clusters display areas of higher concentration of LIHTC units. Click on a cluster or zoom in to see how units are distributed!")),
-    
-            tabPanel("Nashville Marker", leafletOutput("NashvilleMarkerMap"),
-                     fluidRow(
-                         "These markers display each LIHTC property.Click on a marker to see the number of units and the tax dollars allocated!")),
-            tabPanel("Moon", leafletOutput("MoonMap"),
-                     fluidRow(
-                         "There are no LIHTC properties on the moon."))
-        ),
-        h6("This data comes from the HUD National Low Income Housing Tax Credit (LIHTC) Database 1987-2019 Data Dictionary")
-    ))
+           h3("Low-Income Housing Tax Credit Properties Map"),
+           tabsetPanel(
+           tabPanel("Nashville Cluster", leafletOutput("NashvilleClusterMap")),
+           tabPanel("Nashville Marker", leafletOutput("NashvilleMarkerMap")),
+           tabPanel("Moon", leafletOutput("MoonMap"))
+  )
+))
 
 # Define server logic ----
 server <- function(input, output, session) {
-    output$NashvilleClusterMap <- renderLeaflet({
-        leaflet(nashdat) %>%
-            addTiles() %>%
-            addMarkers(lng=~longitude, 
-                       lat=~latitude,
-                       popup=~paste0("Project: ",
-                                     projectname,
-                                     "<br/>Project Address: ",
-                                     proj_add, 
-                                     "<br/>ZIP: ",
-                                     proj_zip, 
-                                     "<br/>Annual dollar amount of tax credits allocated: $",
-                                     allocamt, 
-                                     "<br/>Total Number of Units: ",
-                                     n_units),
-                       clusterOptions = markerClusterOptions())
+  AnchorDown <- makeIcon(
+    iconUrl = "https://www.logolynx.com/images/logolynx/f7/f7d08cb2999bf2d07c1c016555a64f16.png",
+    iconWidth = 70, iconHeight = 35)
+
+  output$NashvilleClusterMap <- renderLeaflet({
+    leaflet(nashdat) %>%
+      addTiles() %>%
+      addMarkers(lng=~longitude, 
+                 lat=~latitude,
+                 popup=~paste0("Project: ",
+                               projectname,
+                               "<br/>Project Address: ",
+                               proj_add, 
+                               "<br/>ZIP: ",
+                               proj_zip, 
+                               "<br/>Annual dollar amount of tax credits allocated: ",
+                               allocamt, 
+                               "<br/>Total Number of Units: ",
+                               n_units),
+                 clusterOptions = markerClusterOptions()) %>%
+    addMarkers(lat=36.14312813278591,
+               lng=-86.80566855798781,
+               icon=AnchorDown,
+               popup='Anchor Down!')
+  })
+  
+  output$NashvilleMarkerMap <- renderLeaflet({
+    leaflet(nashdat) %>%
+      addTiles() %>%
+      addMarkers(lng=~longitude, 
+                 lat=~latitude,
+                 popup=~paste0("Project: ",
+                               projectname,
+                               "<br/>Project Address: ",
+                               proj_add, 
+                               "<br/>ZIP: ",
+                               proj_zip, 
+                               "<br/>Annual dollar amount of tax credits allocated: ",
+                               allocamt, 
+                               "<br/>Total Number of Units: ",
+                               n_units)) %>%
+    addMarkers(lat=36.14312813278591,
+               lng=-86.80566855798781,
+               icon=AnchorDown,
+               popup='Anchor Down!')
     })
-    
-    output$NashvilleMarkerMap <- renderLeaflet({
-        leaflet(nashdat) %>%
-            addTiles() %>%
-            addMarkers(lng=~longitude, 
-                       lat=~latitude,
-                       popup=~paste0("Project: ",
-                                     projectname,
-                                     "<br/>Project Address: ",
-                                     proj_add, 
-                                     "<br/>ZIP: ",
-                                     proj_zip, 
-                                     "<br/>Annual dollar amount of tax credits allocated: $",
-                                     allocamt, 
-                                     "<br/>Total Number of Units: ",
-                                     n_units))})
-    
-    output$MoonMap <- renderLeaflet({
-        leaflet() %>%
-            addTiles('https://s3.amazonaws.com/opmbuilder/301_moon/tiles/w/hillshaded-albedo/{z}/{x}/{y}.png')
-    })
-    
+  
+  output$MoonMap <- renderLeaflet({
+    leaflet() %>%
+      addTiles('https://s3.amazonaws.com/opmbuilder/301_moon/tiles/w/hillshaded-albedo/{z}/{x}/{y}.png')
+  })
+  
 }
 # Run the app ----
 shinyApp(ui = ui, server = server)
